c96067a2c4e2c6e7230fe677d86e6313d0164964
Murthy Karra <karrad@karra.co>
2023-03-28 12:57:47 -0700

    reversed string solutions

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -5,7 +5,42 @@
 //   reverse('apple') === 'leppa'
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
-
-function reverse(str) {}
+//* Solution 4
+function reverse(str) {
+  // return str.split("").reduce((reversed, character) => {
+  //   return character + reversed;
+  // }, "");
+  return str.split("").reduce((rev, char) => char + rev, "");
+  // str = hello
+  // 1st element rev='', char='h'
+  // 2nd element rev = 'h', char='e'
+  // 3rd element rev = 'eh', char='l'
+  // 4th element rev = 'leh', char='l'
+  // 5th element rev = 'lleh', char='o'
+  // Final char+rev = 'olleh'
+}
 
 module.exports = reverse;
+
+//* Solution 1
+// function reverse(str) {
+
+//   const arr = str.split("");
+//   arr.reverse();
+//   return arr.join("");
+// }
+
+//* Solution 2
+// function reverse(str) {
+//   return str.split("").reverse().join("");
+// }
+
+//* Solution 3
+// function reverse(str) {
+//   let reversed = "";
+
+//   for (let character of str) {
+//     reversed = character + reversed;
+//   }
+//   return reversed;
+// }
