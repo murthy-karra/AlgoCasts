fc7d6daac12f7a11c165aa2e3890d6947ff32226
Murthy Karra <karrad@karra.co>
2023-04-02 22:59:02 -0700

    Anagram solutions

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,28 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+  stringA = stringA.replace(/[^\w]/g, "").toLowerCase().split("");
+  stringB = stringB.replace(/[^\w]/g, "").toLowerCase().split("");
+  if (stringA.length != stringB.length) {
+    return false;
+  }
+  const aMap = {};
+  const bMap = {};
+  for (let char of stringA) {
+    aMap[char] = aMap[char] + 1 || 1;
+  }
+  for (let char of stringB) {
+    bMap[char] = bMap[char] + 1 || 1;
+  }
+  console.log(aMap, bMap);
+
+  for (let key in aMap) {
+    if (aMap[key] != bMap[key]) {
+      return false;
+    }
+  }
+  return true;
+}
 
 module.exports = anagrams;
