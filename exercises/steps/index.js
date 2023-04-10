db16e5343c1b25254fb11e325d78804bbb69a1e3
Murthy Karra <karrad@karra.co>
2023-04-09 17:39:34 -0700

    Stairs Recursive Solution

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,18 +17,34 @@
 //       '### '
 //       '####'
 
-function steps(n) {
-  for (let i = 1; i <= n; i++) {
-    let level = "";
-    for (let j = 1; j <= n; j++) {
-      if (j <= i) {
-        level += "#";
-      } else {
-        level += " ";
-      }
-    }
-    console.log(level);
+// function steps(n) {
+//   for (let i = 1; i <= n; i++) {
+//     let level = "";
+//     for (let j = 1; j <= n; j++) {
+//       if (j <= i) {
+//         level += "#";
+//       } else {
+//         level += " ";
+//       }
+//     }
+//     console.log(level);
+//   }
+// }
+function steps(n, row = 0, stair = "") {
+  if (n === row) {
+    return;
   }
+  if (n === stair.length) {
+    console.log(stair);
+    steps(n, row + 1);
+    return;
+  }
+  // if (stair.length <= row) {
+  //   stair += "#";
+  // } else {
+  //   stair += " ";
+  // }
+  //steps(n, row, stair);
+  steps(n, row, stair + (stair.length <= row ? "#" : " "));
 }
-
 module.exports = steps;
